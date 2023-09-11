3e2724558be838cce9e36bd1c3f8f4a6d33c53c6
Notxamer <maxtonleasure@gmail.com>
2023-09-11 12:35:51 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+//First and Last Names ar 2 seperate strings
+string FirstName = "John";
+string LastName = "Doe" ;
+//Conbines First and Last Name
+Console.WriteLine(FirstName + " " + LastName);
+// Writes guess age and the user guesses
+Console.WriteLine("Guess age 1-40: ");
+string val = Console.ReadLine();
+int age;
+age = Convert.ToInt32(val);
+//Writes guessed age and if it is true or false
+Console.WriteLine("Age: " + age); 
+bool TF = age == 32;
+Console.WriteLine(TF); 
+     }
+  }
+}
