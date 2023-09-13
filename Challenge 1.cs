6d60b5433979d8925e862da5e03c7a12a582e86a
Notxamer <maxtonleasure@gmail.com>
2023-09-13 09:01:37 -0600

    Number 1

--- Challenge 1.cs
+++ Challenge 1.cs
@@ -1,4 +1,4 @@
-using System;
+uusing System;
 
 namespace MyApplication
 {
@@ -6,17 +6,23 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
-Console.WriteLine("Enter First Value to Multiply: ");
+        String Name = "Me";
+        double two = 2.2;
+        bool TruFal = true;
+Console.WriteLine("Enter First Value to use: ");
 string val = Console.ReadLine();
+
 int x;
 x = Convert.ToInt32(val);
 
-Console.WriteLine("Enter Second Value to Multiply: ");
+Console.WriteLine("Enter Second Value to use: ");
 string val2 = Console.ReadLine();
 int y;
 y = Convert.ToInt32(val2);
-Console.WriteLine(x*y);
-
+Console.WriteLine("Multiplied: " + x*y);
+Console.WriteLine("Divided: " + x/y);
+Console.WriteLine("Added: " + x+y);
+Console.WriteLine("Remainder: " + x % y);
      }
   }
 }
