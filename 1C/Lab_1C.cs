4882dc5d9c1910f710a606807f9d3ed20f98f4a7
Notxamer <maxtonleasure@gmail.com>
2023-09-20 10:23:44 -0600

    lab1C

---
+++ 1C/Lab_1C.cs
@@ -0,0 +1,24 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//User enters the Pyramid size
+		Console.WriteLine("Enter the size of your pyramid: ");
+		int Size = Convert.ToInt32(Console.ReadLine());
+		int Num = 1;
+		int count = 0;
+		 do
+		 { Console.WriteLine();
+		  do
+		  { Console.Write(Num);
+		   count ++;
+		  } while ( count < Num);
+		  count = 0;
+		  Num ++;
+		 } while (Size >= Num);
+		
+		
+	}
+}
