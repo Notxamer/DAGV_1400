2c77ca87c70a34f56bbf1572645dba22b1c5bcdb
Notxamer <maxtonleasure@gmail.com>
2023-09-20 19:43:02 -0600

    Challenges

---
+++ 1C/Challenge_1C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//Program Picks Random Number 1-10
+		Random rnd = new Random();
+		int Ans = rnd.Next(10);
+		int Gs;
+		
+		//Player gets to guess
+		do
+		{ Console.WriteLine("What is your guess 1-10? ");
+		 Gs = Convert.ToInt32(Console.ReadLine());
+		 if (Gs > Ans)
+			 Console.WriteLine("Too High!");
+		 else if (Gs < Ans)
+			 Console.WriteLine("Too Low!");
+		} while (Gs != Ans);
+		Console.WriteLine("You Win!!");
+		
+		
+		 
+		
+		
+		
+	}
+}
---
+++ 1C/Challenge_2C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//User enters his/hers favorite food
+		Console.WriteLine("Enter your three favorite foods");
+		string[] Foods = {"", "", ""};
+		int Num = 0;
+		do
+		{
+		 Foods[Num] = Console.ReadLine();
+			Num ++;
+		} while (Num <= 2);
+		
+		//Outputs users anwers with add line
+		Console.WriteLine(Foods[0] + " is delicious!");
+		Console.WriteLine("I love myself some " + Foods[1] + "!");
+		Console.WriteLine(Foods[2] + " is my favorite!");
+		
+		
+		
+		 
+		
+		
+		
+	}
+}
