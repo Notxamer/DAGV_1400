4706dd1107c64a2b104904e7fca710857a7d6582
Notxamer <maxtonleasure@gmail.com>
2023-10-10 16:33:18 -0600

    mistake

--- FallClass/Assets/Scenes/PlayerMove.cs
+++ FallClass/Assets/Scenes/PlayerMove.cs
@@ -11,7 +11,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float gravity = 9.81f;
-  
+
 
     private CharacterController Controller;
     private Vector3 moveDirection;
@@ -20,45 +20,46 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Controller = GetComponent<CharacterController>();
+        Controller = GetComponent<CharacterController>();
 
     }
 
-   
+
     // Update is called once per frame
     void Update()
     {
 
 
-      
+
         float Hor = Input.GetAxis("Horizontal");
         float Vert = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(Hor, 0, Vert);
+        moveDirection = new Vector3(Hor, 0, Vert);
+
 
         moveDirection.y -= gravity * Time.deltaTime;
 
-
+       
 
         if (Input.GetButtonDown("Jump") && Controller.isGrounded)
-            {
-                isJumping = true;
-           
-        }
-            else
-            {
-  
-            isJumping = false;
-            }
-
+        {
+            isJumping = true;
             
-
-           
-            if (isJumping)
-            {
-                moveDirection.y = jumpForce;
-               
         }
-        Controller.Move(moveDirection * Time.deltaTime * moveSpeed);
+        else
+        {
+            Controller.Move( Time.deltaTime * moveSpeed * moveDirection);
+            isJumping = false;
+        }
+
+
+
+
+        if (isJumping)
+        {
+          moveDirection.y = jumpForce;
+            Debug.Log("Jumps");
+        }
+      
 
     }
 }
